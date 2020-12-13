cbf3ed4e488025dc1e2f185479579327a8e5751f
Fedde <feddetilma@hotmail.com>
2020-12-13 10:18:52 +0100

    pseudocode for next steps

--- src/views/Detail.vue
+++ src/views/Detail.vue
@@ -1,9 +1,36 @@
 <template>
-  <div>Hello world</div>
+  <div>
+    <div>Hello world</div>
+    <div>Video place</div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "Detailpage"
+  name: "Detail",
+  data() {
+    return {
+      user_id: null,
+      video_id: null,
+      video: {}
+    };
+  },
+  methods: {
+    // update when > 40% watched
+    // update when finished video
+  },
+  created() {
+    // ** video **
+    // get parameter from URL
+    // lookup paratemer in mapping.csv for video id
+    // found or not found
+    // if found: GET request to get the video data
+    // not found: error message
+    // ** user id **
+    // get query parameter user from url (user)
+    // get user_id from local storage
+    // define user id (not required)
+    // what if no userid is defined?
+  }
 };
 </script>
