638abb9b6477aac0bc87bf0e19dc4d79027b520d
Raphaël Claasen <raphael.claasen@gmail.com>
2021-01-21 20:52:11 +0100

    Remove references to turbolinks in code

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,8 +4,8 @@
 // that code so it'll be compiled.
 
 import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
+// import Turbolinks from "turbolinks"
 import "channels"
 
 Rails.start()
-Turbolinks.start()
+// Turbolinks.start()
