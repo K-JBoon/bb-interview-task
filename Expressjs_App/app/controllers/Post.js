e69916cf9226f66af5901ecfd3d6def3cb308a99
Nil Amrutlal <nil.amrutlal@procensus.pt>
2022-04-20 00:48:39 +0100

    Post & User Controller and Model

--- Expressjs_App/app/controllers/Post.js
+++ Expressjs_App/app/controllers/Post.js
@@ -1,10 +1,54 @@
+var request = require('request');
 var Post = require('../models/Post');
+ 
 
-exports.getAllPosts = function(req, res) {
-    console.log("Entrei")
-    
-    Post.getAllPosts(function(err, posts) {
-        if(err) res.send({error: true});
-        else res.send(posts);
+// exports.getPostById = function(req, res) {
+
+//     if(!req.body.postId) {
+//        res.status(400).send({ error: true, message: 'No id provided'});
+//        return;
+//     }
+
+//     Post.getPostById(req.body.postId, function(err, posts) {
+//         console.log("Entrei")
+//     // Post.getPostById(req.query.id, function(err, posts) {
+//         if(err) res.send({error: true});
+//         else res.send(posts);
+//     })
+// }
+
+
+exports.getPostById = function(req, res) {
+
+    if(!req.body.postId) {
+       res.render('viewPost', { title: "Not Found" , body: "Not Found" });
+       return;
+    }
+
+    Post.getPostById(req.body.postId, function(err, result) {
+        if(err) res.render('viewPost', { title: "Not Found" , body: "Not Found" });
+        else {
+            // console.log(result.mediaID);
+            res.render('viewPost', { title: result.postInfo.title , body: result.postInfo.body, mediaID: result.mediaID });
+        }
     })
+}
+
+
+
+
+exports.getPostByIdInternal = async function(id){
+    request(`https://jsonplaceholder.typicode.com/posts?id=${id}`, { json: true }, (err, res, body) => { 
+        if (err) {
+            return{title: "Not Found", body: "Not Found"}; 
+        }
+        else{    
+            console.log("ENTREIII")
+            if(body.length === 0 && body !== 'undefined') return{title: "Not Found", body: "Not Found"};
+            else {
+                console.log("Aqui")
+                return{title: body[0].title, body: body[0].body} 
+            };
+        };
+    });
 }
--- Expressjs_App/app/models/Post.js
+++ Expressjs_App/app/models/Post.js
@@ -1,18 +1,77 @@
 const request = require('request');
+const csv = require('csv-parser');
+const fs = require('fs'); 
 
-var Post = function( ){
-
+var Post = function(userId, id, tittle, body ){
+    this.userId = userId;
+    this.id = id;
+    this.tittle = tittle;
+    this.body = body;
 }
 
 
 
-Post.getAllPosts = function getAllPosts(result) {
-    request('https://jsonplaceholder.typicode.com/posts', { json: true }, (err, res, body) => { 
-        if (err) { result(true, {valid: false}) }
-        else{ result (null, body) };
+Post.getPostById = function getPostById(id, result) {
+
+    getMediaID(id, function(err, resMedia){
+        if(err) result(true, {valid: false});
+        else{
+            request(`https://jsonplaceholder.typicode.com/posts?id=${id}`, { json: true }, (err, res, body) => { 
+            if (err) { result(true, {valid: false}) }
+            else{    
+                if(body.length === 0 && body !== 'undefined') result (false, {valid: false}) 
+                else {
+                    // postInfo: information regarding the post ; mediaID: mediaID clip for the postID
+                    console.log(resMedia.Id);
+                    result(false, {postInfo: body[0], mediaID: resMedia.Id})
+                }
+            };
+        });
+    }
     });
-
 }
+
+
+// Function that retrieves the mediaID from postID
+async function getMediaID(id, result){
+
+    // Read mapping file
+        const filename = 'mapping.csv';
+        const mappingData = []
+    
+        fs.createReadStream(filename)
+        .pipe(csv({ delimiter: ',' }))
+        .on('data', (r) => {
+            // console.log(r);
+            mappingData.push(r);        
+        })
+        .on('end', () => {
+            console.log(`${filename} read successfull`);
+            const foundItem = mappingData.find(item => item.Post == id);
+            result(null, {Id: foundItem.Media});
+        })
+    }
+
+
+
+// if(id) var url = `https://jsonplaceholder.typicode.com/users?id=${id}`;
+// else var url = `https://jsonplaceholder.typicode.com/users`;
+
+// request(url, { json: true }, (err, res, body) => { 
+//     if (err) { result(true, {valid: false}) }
+//     else{ 
+//         // result (null, body[0].address.street);
+//         var responseObject = body[0];
+//         if(body.length === 0 && body !== 'undefined') 
+//             { result(false, {notFound: true})}
+//         var foundUser = new User(
+//             responseObject.id, responseObject.name, responseObject.username, responseObject.email, 
+//             responseObject.address.street, responseObject.address.city, responseObject.phone, 
+//             responseObject.website, responseObject.company.name
+//         )
+//          result (null, foundUser) 
+//     };
+// });
 
 
 
