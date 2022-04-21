3de3da3bbe9abcfd986a009d0ff3647abfdffc29
Nil Amrutlal <nil.amrutlal@procensus.pt>
2022-04-21 01:34:48 +0100

    Not Found view and routing

--- Expressjs_App/app/routes/index.js
+++ Expressjs_App/app/routes/index.js
@@ -3,37 +3,16 @@ module.exports = function(app){
 
 // Objects
     var Posts   = require('../controllers/Post');
-    var User    = require('../controllers/User');
 
 
-    app.get('/', function(req, res){
-        res.send("Hello World")
+    app.get('/Alive', function(req, res){
+        res.send("Yes, I am alive")
     });
 
-
-    app.get('/login', function(req, res){
-        res.render('login');
-    })
-
-    // app.route('/posts').get(Posts.getAllPosts);
-    // app.route('/posts').get(Posts.getPostById);
     app.route('/postId').post(Posts.getPostById);
-
     app.route('/post').get(Posts.getPost);
 
-    // app.get('/post', (req, res) => {
-    //     res.render('post');
-    // });
-
-
-    // app.post('/postId', (req, res) => {
-    //     var Post = await Posts.getPostByIdInternal(req.body.postId);
-    //     console.log(Post);
-    //     res.render('viewPost', { title: Post.title , body: Post.body });
-    // });
-
-
-    app.route('/user').get(User.getUserById);
+    app.get('*', (req, res) => {res.render('notFound')});
 
 
 
