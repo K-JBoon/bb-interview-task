5fe3523d01493bb5bffc52201ea143325e077cdf
Nil Amrutlal <32678647+nil-amrutlal@users.noreply.github.com>
2022-04-21 12:57:33 +0100

    Revert "Nil assignment"

--- Expressjs_App/app/controllers/User.js
+++
@@ -1,9 +0,0 @@
-// var User = require('../models/User');
-
-// exports.getUserById = function(req, res) {
-
-//     User.getUserById(req.query.id, function(err, user) {
-//         if(err) res.send({error: true});
-//         else res.send(user);
-//     })
-// }
